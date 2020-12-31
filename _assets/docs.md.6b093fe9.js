0642b4f0f71dbdae0b8f7189040bdba9774f352e
CrazyMrYan <2407488005@qq.com>
2020-12-31 10:24:04 +0800

    init blog

---
+++ _assets/docs.md.6b093fe9.js
@@ -0,0 +1,1 @@
+import{g as t,j as e,f as r,q as a}from"./common-d6bd45c0.js";const o='{"title":"归档","frontmatter":{"page":true,"date":"2020-11-13","title":"归档","describe":"归档"},"relativePath":"docs.md","lastUpdated":1609316600952.3896}';var d={};d.render=function(o,d,s,n,c,i){const l=a("Docs");return r(),t("div",null,[e(l)])};export default d;export{o as __pageData};
---
+++ _assets/index.md.be620e42.js
@@ -0,0 +1,1 @@
+import{g as e,f as t}from"./common-d6bd45c0.js";const r='{"title":"Home","frontmatter":{"page":true,"home":true,"date":"2020-11-13","title":"首页","describe":"首页"},"relativePath":"index.md","lastUpdated":1609316600953.3872}';var a={};a.render=function(r,a,d,o,i,m){return t(),e("div")};export default a;export{r as __pageData};
